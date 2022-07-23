657badfea899d8c547728d7dbbe1c62f37aa9b21
Ivan Mišák <info@ivanmisak.eu>
2022-07-23 10:13:03 +0200

    config init file

---
+++ app/config/yourhost.example.php
@@ -0,0 +1,14 @@
+<?php
+
+ini_set( 'display_errors', 0 );
+error_reporting( 0 );
+
+define( 'APP_DEBUG' , false );
+define( 'APP_LANG', 'en' );
+define( 'APP_URL' , 'https://example.com' );
+
+define( 'DB_HOST', 'localhost' );
+define( 'DB_PORT', 3306 );
+define( 'DB_NAME', '--' );
+define( 'DB_USER', '--' );
+define( 'DB_PASSWORD', '--' );
